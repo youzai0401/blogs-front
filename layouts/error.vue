04d825a4dc0859195c5546d85708623c42eb1edd
zhouzhiwen <825142772@qq.com>
2018-05-30 10:31:11 +0800

    init

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    {{error}}
+    <h1 v-if="error.statusCode === 404">页面不存在</h1>
+    <h1 v-else>应用发生错误异常</h1>
+    <nuxt-link to="/">首 页</nuxt-link>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['error']
+  }
+</script>
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="container">
+    <div>
+      <!--<app-logo/>-->
+      <!--<testq/>-->
+      <h1 class="title">
+        nuxt-blog-node
+      </h1>
+      <h2 class="subtitle">
+        blog-ssr
+        blog-ssr
+        blog-ssr
+        blog-ssr
+        blog-ssr
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+      {{name}}
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import testq from '~/components/testq.vue'
+
+
+export default {
+  components: {
+    AppLogo,testq
+  },
+  transition: 'test',
+  asyncData (context) {
+    // called every time before loading the component
+    return { name: 'World' }
+  },
+  fetch () {
+    // The fetch method is used to fill the store before rendering the page
+  },
+//  head () {
+//    // Set Meta Tags for this Page
+//  },
+  // and more functionality to discover
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
